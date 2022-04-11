6cf2e47d2ab467a71eaa84161ad46340086a59ec
BZAgustin <abustoszanelli@gmail.com>
2022-04-11 11:31:49 -0300

    - Add functionality to 'other' buttons

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 let firstOperand = 0;
 let lastOperand = 0;
 let operator = '';
-let isFloat = false;
 
 // Target display and buttons
 const display = document.querySelector('.screen');
@@ -11,10 +10,32 @@ const operators = Array.from(document.querySelectorAll('.operator'));
 const other = Array.from(document.querySelectorAll('.other'));
 const result = Array.from(document.querySelector('.result'));
 
-// Add event listeners
+// Add number buttons' event listeners
 numbers.forEach(btn => {
     btn.addEventListener('click', e => {
         drawNumber(e);
+    });
+});
+
+other.forEach(btn => {
+    btn.addEventListener('click', e => {
+        let id = e.target.id;
+        
+        if(id === 'AC') {
+            clearValues();
+        } else if(id === 'C') {
+            if(Array.from(display.textContent).length < 2) {
+                display.textContent = '0';
+            } else if(display.textContent != '0') {
+                display.textContent = deleteLastDigit(display.textContent);
+            } else return;
+        } else if(id === 'posneg') {
+            if(display.textContent != '0') {
+                display.textContent = changeSign(display.textContent);
+            } else return;
+        } else if(id === 'decimal') {
+            display.textContent = toFloat(display.textContent);
+        }
     });
 });
 
@@ -65,11 +86,18 @@ function clearValues() {
     lastOperand = 0;
     operator = '';
     isFloat = false;
+    display.textContent = '0';
 }
 
 // Remove last digit
 function deleteLastDigit(str) {
-    return str.pop();
+    let arr = Array.from(str);
+    console.log(arr);
+    arr.pop();
+    console.log(arr);
+    str = arr.join("");
+    console.log(str);
+    return str;
 }
 
 // Change to opposite sign
@@ -79,5 +107,15 @@ function changeSign(val) {
   } else if(val < 0) {
       return Math.abs(val);
   } else return val;
+}
+
+// Change to float
+function toFloat(str) {
+    // Find out if there's already a '.' in the displayed value
+    if(!Array.from(str).some(element => element === '.')) {
+        return str + '.';
+    } else {
+        return str;
+    }
 }
 
