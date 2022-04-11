bc1827a6b664cff82485ec8569748d0ab8b5d7d9
BZAgustin <abustoszanelli@gmail.com>
2022-04-11 18:33:24 -0300

    - Wire operators and add basic functionality

--- script.js
+++ script.js
@@ -13,7 +13,14 @@ const result = Array.from(document.querySelector('.result'));
 // Add number buttons' event listeners
 numbers.forEach(btn => {
     btn.addEventListener('click', e => {
-        drawNumber(e);
+        if(operator != '') {
+            display.textContent = '';
+            drawNumber(e);
+            operator = '';
+        } else {
+            drawNumber(e);
+        }
+        
     });
 });
 
@@ -21,8 +28,10 @@ other.forEach(btn => {
     btn.addEventListener('click', e => {
         let id = e.target.id;
         
+        // Select which action to perform based on the button pressed
         if(id === 'AC') {
             clearValues();
+            display.textContent = '0';
         } else if(id === 'C') {
             if(Array.from(display.textContent).length < 2) {
                 display.textContent = '0';
@@ -38,6 +47,50 @@ other.forEach(btn => {
         }
     });
 });
+
+operators.forEach(btn => {
+    btn.addEventListener('click', e => {
+        let id = e.target.id;
+        
+        // Check if there's an operation already going
+        if(operator != '') {
+            console.log('Operator is defined');
+            // Check whether the operation button is being pressed twice
+            if(lastOperand === 0) {
+                console.log('No last operand. Returning');
+            } else {
+                console.log('Operation and last operand found. Displaying solved operation');
+                display.textContent = solveOperation(operator, firstOperand, lastOperand);
+            }
+        }
+        
+        if(id === 'add') {
+            console.log('Current operator: plus');
+            operator = '+';
+        } else if(id === 'substract') {
+            console.log('Current operator: minus');
+            operator = '-';
+        } else if(id === 'multiply') {
+            console.log('Current operator: times');
+            operator = '*';
+        } else {
+            console.log('Current operator: by');
+            operator = '/';
+        } 
+        
+        if(display.textContent != '0') {
+            if(firstOperand === 0) {
+                console.log('No first operand found. Saving display as 1st operand');
+                firstOperand = display.textContent;
+            } else {
+                console.log('First operand found. Saving display as 2nd operand and solving operation');
+                lastOperand = display.textContent;
+                display.textContent = solveOperation(operator, firstOperand, lastOperand);
+            }
+        } else return;
+        
+    });
+})
 
 // Draw number on screen
 function drawNumber(e) {
@@ -70,14 +123,42 @@ function multiply(a, b) {
 }
 
 // Operator function
-function operate(operator, x, y) {
+function solveOperation(operator, x, y) {
+    // Check whether the number to evaluate will be an int or a float
+    if(isFloat(x) && isFloat(y)) {
+        x = parseFloat(x);
+        y = parseFloat(y);
+    } else if(isFloat(x)) {
+        x = parseFloat(x);
+        y = parseInt(y);
+    } else if(isFloat(y)) {
+        x = parseInt(x);
+        y = parseFloat(y);
+    } else {
+        x = parseInt(x);
+        y = parseInt(y);
+    }
+
+    console.log(`First operand: ${x}`);
+    console.log(`Second operand: ${y}`);
+    
     if(operator === '+') {
+        display.textContent = add(x, y);
+        clearValues();
         return add(x, y);
     } else if(operator === '-') {
+        display.textContent = substract(x, y);
+        clearValues();
         return substract(x, y);
     } else if (operator === '/') {
+        display.textContent = divide(x, y);
+        clearValues();
         return divide(x, y);
-    } else return multiply(x, y);
+    } else {
+        display.textContent = multiply(x, y);
+        clearValues();
+        return multiply(x, y);
+    }
 }
 
 // Clear all values
@@ -85,18 +166,13 @@ function clearValues() {
     firstOperand = 0;
     lastOperand = 0;
     operator = '';
-    isFloat = false;
-    display.textContent = '0';
 }
 
 // Remove last digit
 function deleteLastDigit(str) {
     let arr = Array.from(str);
-    console.log(arr);
     arr.pop();
-    console.log(arr);
     str = arr.join("");
-    console.log(str);
     return str;
 }
 
@@ -111,11 +187,14 @@ function changeSign(val) {
 
 // Change to float
 function toFloat(str) {
-    // Find out if there's already a '.' in the displayed value
-    if(!Array.from(str).some(element => element === '.')) {
+    if(!isFloat(str)) {
         return str + '.';
     } else {
         return str;
     }
 }
 
+// Evaluate float
+function isFloat(val) {
+    return Array.from(val).some(element => element === '.');
+}
